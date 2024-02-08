679842d92cdfe77b5077da9b112ba12c1969e6d9
emrahhakan <emrahhakanagan@gmail.com>
2024-02-08 23:37:36 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## --- NETOLOGY HOMEWORK ---
+### JAVA CORE --> Testing Codes
+
+#### --> Task 2 --> Using JUnit with Hamcrest
---
+++ src/main/java/org/example/Employee.java
@@ -0,0 +1,52 @@
+package org.example;
+
+public class Employee {
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String country;
+    private int age;
+
+    public Employee() {
+    }
+
+    public Employee(long id, String firstName, String lastName, String country, int age) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.country = country;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", country='" + country + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
+
